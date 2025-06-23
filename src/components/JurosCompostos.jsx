cd3343bc83dbb4e5d9c6f6a6efb05de94e3701ae
RyanAmboni <ryan.candeu.amboni@gmail.com>
2025-06-23 14:36:33 -0300

    Update JurosCompostos.jsx

--- src/components/JurosCompostos.jsx
+++ src/components/JurosCompostos.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; // LINHA CORRIGIDA
 import { useToast } from '../contexts/ToastContext';
 
 const JurosCompostos = () => {
