873d5be2d8338aec90ff1ff8e3c5c2b4416b880d
RyanAmboni <ryan.candeu.amboni@gmail.com>
2025-06-23 14:40:41 -0300

    Update SimuladorCompra.jsx

--- src/components/SimuladorCompra.jsx
+++ src/components/SimuladorCompra.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useToast } from '../contexts/ToastContext';
 
 const getCalculadoraData = async () => {
